10a91a365bf2dfa961f86dc66fd93ebc070a7607
maatheusgouveia <matheus_guitarplayer@hotmail.com>
2021-01-28 00:33:20 -0300

    aula 02

--- public/pages/room/src/view.js
+++ public/pages/room/src/view.js
@@ -37,4 +37,12 @@ class View {
 		const videoGrid = document.getElementById('video-grid');
 		videoGrid.append(div);
 	}
+
+	setParticipants(count) {
+		const myself = 1;
+
+		const participants = document.getElementById('participants');
+
+		participants.innerHTML = count + myself;
+	}
 }
